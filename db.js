37b514fd486b934d23343b956b3e60cd8c5ba1a1
va-z <vasilizadorojnyuk@gmail.com>
2021-05-18 13:46:13 +0500

    init: start task

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
 const Sequelize = require('sequelize');
                                 //database username   password
-const sequelize = new Sequelize('gamedb', 'postgres', 'ghastb0i', {
-    host: 'localhost',
-    dialect: 'postgres'
-})
+const sequelize = new Sequelize(
+    process.env.DB, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, 
+    {
+        host: 'localhost',
+        dialect: 'postgres'
+    }
+)
 
 sequelize.authenticate().then(
     function success() {
