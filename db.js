519349a16f08c8bae1cace00d01aa2aeec81ccc0
va-z <vasilizadorojnyuk@gmail.com>
2021-05-18 23:16:55 +0500

    refactor: add operatorsAliases: Sequelize.Op to db.js

--- db.js
+++ db.js
@@ -1,14 +1,10 @@
 const Sequelize = require('sequelize');
                                 //database username   password
-const sequelize = new Sequelize(
-    process.env.DB, 
-    process.env.DB_USER, 
-    process.env.DB_PASSWORD, 
-    {
-        host: 'localhost',
-        dialect: 'postgres'
-    }
-)
+const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    operatorsAliases: Sequelize.Op,
+})
 
 sequelize.authenticate().then(
     function success() {
