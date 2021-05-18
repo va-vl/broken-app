da729f149bac846b20ca2bfd3f30c8e5608dd414
va-z <vasilizadorojnyuk@gmail.com>
2021-05-19 02:42:32 +0500

    chore: remove operatorsAliases flag (leave for refactoring)

--- db.js
+++ db.js
@@ -3,7 +3,6 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
-    operatorsAliases: Sequelize.Op,
 })
 
 sequelize.authenticate().then(
