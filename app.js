32364abe526c69995714ffd74f6b5a28dd95a963
va-z <vasilizadorojnyuk@gmail.com>
2021-05-18 18:48:26 +0500

    fix: add port to app.listen

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.use(require('body-parser'));
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
-app.listen(function() {
+app.listen(4000, function() {
     console.log("App is listening on 4000");
-})
+});
