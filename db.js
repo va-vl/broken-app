2420db844ac4ee98f702e38e15abf14fa17f5e99
va-z <vasilizadorojnyuk@gmail.com>
2021-05-19 15:01:33 +0500

    fix: fix deprecated operatorsAliases

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    operatorsAliases: Sequelize.Op,
 })
 
 sequelize.authenticate().then(
